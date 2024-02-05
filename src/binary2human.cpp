31a007195663c5627874af677e84da8e1147ccfc
NerKnutson <noahtknutson@gmail.com>
2024-02-05 14:20:07 -0600

    Fixed human2binary and binary2human to take multiple types (double, complex double, integer). This solves the r2cFFT to c2rFFT debugging issue.

--- src/binary2human.cpp
+++ src/binary2human.cpp
@@ -1,23 +1,57 @@
 #include <iostream> // std::cerr std::cout
 #include <iomanip> // std::setprecision()
-#include <limits> // numeric_limits<>
+#include <limits> // std::numeric_limits<>
+#include <cstring> // std::strcmp()
+#include <vector> // std::vector<>
+#include <complex> // std::complex<>
 using namespace std;
+void routine(vector<double>);
+void routine(vector<complex<double>>);
+void routine(vector<int>);
 
 int main(int argc, char* argv[]) {
-	if(argc < 1 || argc > 2){
-		cerr << "Usage: " << argv[0] << "[optional: <number of channels>]" << endl;
+	if(argc < 1 || argc > 5){
+		cerr << "Usage: " << argv[0] << "[optional: --N_channel <number of channels>] [optional: --type <numeric type>]" << endl;
 		return 1;
 	}
+
 	unsigned N_channel = 1;
-	if(argc == 2)
-		N_channel = atoi(argv[1]);
+	for(int arg = 0; arg < argc; ++arg)
+		if(strcmp(argv[arg], "--N_channel") == 0 || strcmp(argv[arg], "-Nc") == 0)
+			sscanf(argv[arg + 1], "%d", &N_channel);
 
-	double data[N_channel];
-	fill(data, data + N_channel, 0.0);
+	// Scans command-line arguments for type
+	for(int arg = 0; arg < argc; ++arg) {
+		if(strcmp(argv[arg], "--type") == 0 || strcmp(argv[arg], "-t") == 0) {
+			if(strcmp(argv[arg + 1], "double") == 0) {
+				vector<double> data(N_channel,0);
+				routine(data);
+				return 0;
+			} else if(strcmp(argv[arg + 1], "std::complex<double>") == 0 || strcmp(argv[arg + 1], "complex double") == 0 || strcmp(argv[arg + 1], "complex") == 0) {
+				vector<complex<double>> data(N_channel,0);
+				routine(data);
+				return 0;
+			} else if(strcmp(argv[arg + 1], "integer") == 0 || strcmp(argv[arg + 1], "int") == 0) {
+				vector<int> data(N_channel,0);
+				routine(data);
+				return 0;
+			}
+		}
+	}
 
+	// If no type; then assume double
+	vector<double> data(N_channel,0);
+	routine(data);
+
+	return 0;
+}
+
+// Routines
+void routine(vector<double> data) {
+	unsigned N_channel = data.size();
 	unsigned indexData = 0;
 	cout << setprecision(numeric_limits<double>::digits10 + 1);
-	while(fread(data, N_channel, sizeof(double), stdin)) {
+	while(fread(&data[0], N_channel, sizeof(double), stdin)) {
 		for(int c = 0; c < N_channel; ++c) {
 			cout << data[c];
 			if(c == N_channel - 1)
@@ -26,6 +60,34 @@ int main(int argc, char* argv[]) {
 				cout << "\t";
 		}
 	}
+}
 
-	return 0;
+void routine(vector<complex<double>> data) {
+	unsigned N_channel = data.size();
+	unsigned indexData = 0;
+	cout << setprecision(numeric_limits<double>::digits10 + 1);
+	while(fread(&data[0], N_channel, sizeof(std::complex<double>), stdin)) {
+		for(int c = 0; c < N_channel; ++c) {
+			cout << data[c];
+			if(c == N_channel - 1)
+				cout << endl;
+			else
+				cout << "\t";
+		}
+	}
+}
+
+void routine(vector<int> data) {
+	unsigned N_channel = data.size();
+	unsigned indexData = 0;
+	cout << setprecision(numeric_limits<double>::digits10 + 1);
+	while(fread(&data[0], N_channel, sizeof(int), stdin)) {
+		for(int c = 0; c < N_channel; ++c) {
+			cout << data[c];
+			if(c == N_channel - 1)
+				cout << endl;
+			else
+				cout << "\t";
+		}
+	}
 }
--- src/human2binary.cpp
+++ src/human2binary.cpp
@@ -1,25 +1,81 @@
 #include <iostream> // std::cerr std::cout
+#include <cstring> // std::strcmp()
+#include <vector> // std::vector<>
+#include <complex> // std::complex<>
 using namespace std;
+void routine(vector<double>);
+void routine(vector<complex<double>>);
+void routine(vector<int>);
 
 int main(int argc, char* argv[]) {
-	if(argc < 1 || argc > 2){
-		cerr << "Usage: " << argv[0] << "[optional: <number of channels>]" << endl;
+	if(argc < 1 || argc > 5){
+		cerr << "Usage: " << argv[0] << "[optional: --N_channel <number of channels>] [optional: --type <numeric type>]" << endl;
 		return 1;
 	}
+
 	unsigned N_channel = 1;
-	if(argc == 2)
-		N_channel = atoi(argv[1]);
+	for(int arg = 0; arg < argc; ++arg)
+		if(strcmp(argv[arg], "--N_channel") == 0 || strcmp(argv[arg], "-Nc") == 0)
+			sscanf(argv[arg + 1], "%d", &N_channel);
 
-	double data[N_channel];
-	fill(data, data + N_channel, 0.0);
+	// Scans command-line arguments for type
+	for(int arg = 0; arg < argc; ++arg) {
+		if(strcmp(argv[arg], "--type") == 0 || strcmp(argv[arg], "-t") == 0) {
+			if(strcmp(argv[arg + 1], "double") == 0) {
+				vector<double> data(N_channel,0);
+				routine(data);
+				return 0;
+			} else if(strcmp(argv[arg + 1], "std::complex<double>") == 0 || strcmp(argv[arg + 1], "complex double") == 0 || strcmp(argv[arg + 1], "complex") == 0) {
+				vector<complex<double>> data(N_channel,0);
+				routine(data);
+				return 0;
+			} else if(strcmp(argv[arg + 1], "integer") == 0 || strcmp(argv[arg + 1], "int") == 0) {
+				vector<int> data(N_channel,0);
+				routine(data);
+				return 0;
+			}
+		}
+	}
 
+	// If no type; then assume double
+	vector<double> data(N_channel,0);
+	routine(data);
+	return 0;
+}
+
+// Routines
+void routine(vector<double> data) {
+	unsigned N_channel = data.size();
 	unsigned indexData = 0;
 	while(cin >> data[indexData]) {
 		indexData++;
 		if(indexData % N_channel == 0) {
 			indexData = 0;
-			fwrite(data, N_channel, sizeof(double), stdout);
+			fwrite(&data[0], N_channel, sizeof(double), stdout);
 		}
 	}
-	return 0;
+}
+
+void routine(vector<complex<double>> data) {
+	unsigned N_channel = data.size();
+	unsigned indexData = 0;
+	while(cin >> data[indexData]) {
+		indexData++;
+		if(indexData % N_channel == 0) {
+			indexData = 0;
+			fwrite(&data[0], N_channel, sizeof(complex<double>), stdout);
+		}
+	}
+}
+
+void routine(vector<int> data) {
+	unsigned N_channel = data.size();
+	unsigned indexData = 0;
+	while(cin >> data[indexData]) {
+		indexData++;
+		if(indexData % N_channel == 0) {
+			indexData = 0;
+			fwrite(&data[0], N_channel, sizeof(int), stdout);
+		}
+	}
 }
